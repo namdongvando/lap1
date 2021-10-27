2096d8ade5620d1edcc4a7b71778e68380e7de8a
namdongvando <32663464+namdongvando@users.noreply.github.com>
2021-10-27 20:10:41 +0700

    upcode

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-
-echo "xin chào";
-echo "abc";
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include './Pages/home.php';
+include './vendor/autoload.php'; 
+Imagesoptimizer\Images::Optimizer("app-hero-bg.jpg"); 
+?>
+ 
+<img width="600" src="app-hero-bg.jpg" alt=""/>
